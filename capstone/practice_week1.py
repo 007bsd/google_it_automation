997475b39edee8d8d2a0a092ace297ecb5bc01cf
007bsd <007bsd>
2020-06-11 22:38:36 +0300

    flipp image

--- capstone/practice_week1.py
+++ capstone/practice_week1.py
@@ -1,4 +1,5 @@
 from PIL import Image
 im = Image.open("example.jpg")
-new_im = im.resize((640,480))
+new_im = im.resize((640, 480))
 new_im.save("example_resized.jpg")
+im.rotate(180).resize((640, 480)).save("flipped_and_resized.jpg")
